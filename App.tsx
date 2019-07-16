24b964c85177fea12a81263d65d8cee0f5418134
Hyungjun Kim <hyungjun.kim@cosmochain.io>
2019-07-16 23:45:23 +0900

    react-native-webview installed

--- App.tsx
+++ App.tsx
@@ -1,13 +1,11 @@
 import React from "react";
-import { View, Text } from "react-native";
 import { createStackNavigator, createAppContainer } from "react-navigation";
+import WebView from "react-native-webview";
 
 class HomeScreen extends React.Component {
   render() {
     return (
-      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-        <Text>Home Screen</Text>
-      </View>
+      <WebView source={{uri: "https://google.com"}}/>
     );
   }
 }
