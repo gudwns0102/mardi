ac5b5258c8717a586a0ee46a4a719dfa770f36e0
Hyungjun Kim <hyungjun.kim@cosmochain.io>
2019-07-16 23:50:50 +0900

    react-native-shadow installed

--- App.tsx
+++ App.tsx
@@ -1,10 +1,21 @@
 import React from "react";
 import { createStackNavigator, createAppContainer } from "react-navigation";
+import { BoxShadow } from "react-native-shadow";
 
+const createShadowOpt = (color: string) => ({
+  width: 196,
+  height: 64,
+  color,
+  border: 8,
+  radius: 14,
+  opacity: 0.6,
+  x: 0,
+  y: 3
+});
 class HomeScreen extends React.Component {
   render() {
     return (
-      <></>
+      <BoxShadow setting={createShadowOpt("#FFAD00")} />
     );
   }
 }
