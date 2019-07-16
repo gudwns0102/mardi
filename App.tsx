03c113e7b08084e2b70f159b1bed25c0bd3e61dc
Hyungjun Kim <hyungjun.kim@cosmochain.io>
2019-07-16 23:48:16 +0900

    react-native-svg installed

--- App.tsx
+++ App.tsx
@@ -1,11 +1,10 @@
 import React from "react";
 import { createStackNavigator, createAppContainer } from "react-navigation";
-import WebView from "react-native-webview";
 
 class HomeScreen extends React.Component {
   render() {
     return (
-      <WebView source={{uri: "https://google.com"}}/>
+      <></>
     );
   }
 }
