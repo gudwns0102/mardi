f1ccdce9ba17bf1fc5a26eab231609f04f80f780
Hyungjun Kim <hyungjun.kim@cosmochain.io>
2019-07-16 23:55:51 +0900

    react-native-linear-gradient installed

--- App.tsx
+++ App.tsx
@@ -1,21 +1,22 @@
 import React from "react";
 import { createStackNavigator, createAppContainer } from "react-navigation";
 import { BoxShadow } from "react-native-shadow";
+import LinearGradient from "react-native-linear-gradient";
+import styled from "styled-components/native";
+const Container = styled(LinearGradient).attrs({
+  colors: ["rgb(23, 135, 217)", "rgb(25, 86, 212)"]
+})`
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  flex: 1;
+  padding: 178px 52px 21px;
+`;
 
-const createShadowOpt = (color: string) => ({
-  width: 196,
-  height: 64,
-  color,
-  border: 8,
-  radius: 14,
-  opacity: 0.6,
-  x: 0,
-  y: 3
-});
 class HomeScreen extends React.Component {
   render() {
     return (
-      <BoxShadow setting={createShadowOpt("#FFAD00")} />
+      <Container />
     );
   }
 }
